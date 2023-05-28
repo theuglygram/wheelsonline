4a5f749b1678e4dc88772fdea8a6707488480577
theuglygram <iamerheren@gmail.com>
2023-05-28 09:47:57 -0700

    styled component  modified

--- app/components/navigation/header.js
+++ app/components/navigation/header.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
 function Header() {
   return (
     <>
--- app/styling/styled-components/globalstyle.js
+++ app/styling/styled-components/globalstyle.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+
 export const Button = styled.button`
   border: none;
   outline: none;
