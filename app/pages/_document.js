85c1a70743c7c408816bb553fdcdc31085df4a04
theuglygram <iamerheren@gmail.com>
2023-05-28 09:11:04 -0700

    document modified

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -43,10 +43,48 @@
 //   );
 // }
 
-import Document from "next/document";
+// import Document from "next/document";
+// import { ServerStyleSheet } from "styled-components";
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const sheet = new ServerStyleSheet();
+//     const originalRenderPage = ctx.renderPage;
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         });
+
+//       const initialProps = await Document.getInitialProps(ctx);
+//       return {
+//         ...initialProps,
+//         styles: [initialProps.styles, sheet.getStyleElement()],
+//       };
+//     } finally {
+//       sheet.seal();
+//     }
+//   }
+// }
+
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -61,7 +99,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       sheet.seal();
